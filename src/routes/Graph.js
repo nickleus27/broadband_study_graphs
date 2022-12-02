af955662aebb505db378960cf48a5b424a8501b5
nickleus27 <nickleus27@gmail.com>
2022-12-01 16:20:15 -0800

    Added ResponsiveContainer, angled x-axis labels

--- src/routes/Graph.js
+++ src/routes/Graph.js
@@ -2,7 +2,7 @@ import '../App.css';
 //import '../components/OptionItem.css';
 //import OptionItem from '../components/OptionItem.js';
 import { useLocation } from 'react-router-dom';
-import { BarChart, XAxis, YAxis, Tooltip, Legend, Bar } from 'recharts';
+import { ResponsiveContainer, BarChart, XAxis, YAxis, Tooltip, Legend, Bar } from 'recharts';
 import { getCarrierModelServerGraph } from '../library/dataToGraph';
 import CustomTooltip from '../components/CustomToolTip';
 
@@ -19,13 +19,22 @@ function Graph() {
         <div className="App">
             <header className="App-header">
                 <h2>Graph For {location.state.title}</h2>
-                <BarChart width={1300} height={700} data={data}>
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip content={CustomTooltip} />
-                    <Legend />
-                    <Bar dataKey="value" fill="#8884d8" />
-                </BarChart>
+                <div style={{ width: '100%', height: 750 }}>
+                    <ResponsiveContainer width="95%" height="80%">
+                        <BarChart data={data} margin={{
+                            top: 11,
+                            right: 0,
+                            left: 21,
+                            bottom: 32,
+                        }}>
+                            <XAxis dataKey="name" interval={0} angle={-11} textAnchor="end" />
+                            <YAxis />
+                            <Tooltip content={CustomTooltip} />
+                            <Legend />
+                            <Bar dataKey="value" fill="#8884d8" />
+                        </BarChart>
+                    </ResponsiveContainer>
+                </div>
             </header>
         </div>
     );
