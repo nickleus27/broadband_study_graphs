8add44576d332fe63fc0a6f995ab3d3b9a09e326
nickleus27 <nickleus27@gmail.com>
2022-12-27 17:11:34 -0600

    refactored onChangeEvents, Graph2 selection working, ready to implement comparison graph

--- src/routes/TestOptions.js
+++ src/routes/TestOptions.js
@@ -1,40 +1,33 @@
-import logo from '../calspeed.png';
+import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import '../App.css';
+import logo from '../calspeed.png';
 import '../components/OptionItem.css';
 import OptionItem from '../components/OptionItem.js';
-import { useLocation } from 'react-router-dom';
-import { useState } from 'react';
-
-/*
-function createButtons(props) {
-  var buttons = [];
-  for (const carrier of Object.keys(props.data[props.title.rounds])) {
-    let buttonData = {};
-    buttonData.route = "/phone_models";
-    buttonData.data = props.data;
-    buttonData.title = Object.assign({}, props.title);
-    buttonData.title.carrier = carrier;
-    buttons.push(
-      <OptionItem
-        key={carrier}
-        name={carrier}
-        description={"Broadband testing for " + carrier}
-        data={buttonData}
-        choice={"Go To " + carrier}>
-      </OptionItem>);
-  }
-  return buttons;
-}
-*/
 
 function TestOptions() {
   const location = useLocation();
   const [carrier, setCarrier] = useState("");
+  const [carrier2, setCarrier2] = useState("");
   const [phone_model, setPhoneModel] = useState("");
+  const [phone_model2, setPhoneModel2] = useState("");
   const [server, setServer] = useState("");
+  const [server2, setServer2] = useState("");
 
   const onChangedCarrier = (event) => {
     setCarrier(event.target.value);
+    setPhoneModel("");
+    setServer("");
+    setCarrier2("");
+    setPhoneModel2("");
+    setServer2("");
+    console.log(event.target.value);
+  }
+
+  const onChangedCarrier2 = (event) => {
+    setCarrier2(event.target.value);
+    setPhoneModel2("");
+    setServer2("");
     console.log(event.target.value);
   }
 
@@ -43,106 +36,123 @@ function TestOptions() {
     console.log(event.target.value);
   }
 
+  const onChangedPhoneModel2 = (event) => {
+    setPhoneModel2(event.target.value);
+    console.log(event.target.value);
+  }
+
   const onChangedServer = (event) => {
     setServer(event.target.value);
     console.log(event.target.value);
   }
 
+  const onChangedServer2 = (event) => {
+    setServer2(event.target.value);
+    console.log(event.target.value);
+  }
+
   const radioOptions = (data, round, optionNum) => {
-    return <div className="Radio-col-container">{_radioOptions(data, round, optionNum)}</div>;
+    if (optionNum === '1') {
+      return <div className="Radio-col-container">{_radioOptions(data, round, optionNum)}</div>;
+    } else {
+      return (
+        <div>
+          <div>Graph 2 Options:</div>
+          <div className="Radio-col-container">{_radioOptions(data, round, optionNum)}</div>
+        </div>
+      );
+    }
   }
 
   const _radioOptions = (data, round, optionNum) => {
-    if (carrier === "") {
-      return carrierOptions(data, round, optionNum);
-    }
     var radioButtons = [];
-    radioButtons.push(carrierOptions(data, round, optionNum));
-    var formPhones = phoneModelOptions(data, round, carrier, optionNum);
-    if (formPhones !== null) {
-      radioButtons.push(formPhones);
-    }
-    if (phone_model === "") {
+    if (optionNum === '1') {
+      radioButtons.push(carrierOptions(data, round, optionNum));
+      if (carrier === "") {
+        return radioButtons;
+      }
+      radioButtons.push(phoneModelOptions(data, round, carrier, optionNum));
+      if (phone_model === "") {
+        return radioButtons;
+      }
+      radioButtons.push(serverOptions(data, round, carrier, phone_model, optionNum));
+      if (server === "") {
+        return radioButtons;
+      }
+      radioButtons.push(createButton(data, round, carrier, phone_model, server, optionNum));
+      return radioButtons;
+    } else if (optionNum === '2' && server !== "") {
+      radioButtons.push(carrierOptions(data, round, optionNum));
+      if (carrier2 === "") {
+        return radioButtons;
+      }
+      radioButtons.push(phoneModelOptions(data, round, carrier2, optionNum));
+      if (phone_model2 === "") {
+        return radioButtons;
+      }
+      radioButtons.push(serverOptions(data, round, carrier2, phone_model2, optionNum));
+      if (server2 === "") {
+        return radioButtons;
+      }
+      radioButtons.push(createButton(data, round, carrier2, phone_model2, server2, optionNum));
       return radioButtons;
     }
-    var formServers = serverOptions(data, round, carrier, phone_model, optionNum);
-    if (formServers !== null) {
-      radioButtons.push(formServers);
-    }
-    if (server === "") {
-      return radioButtons;
-    }
-    radioButtons.push(createButton(data, round, carrier, phone_model, server, optionNum));
-    return radioButtons;
   }
 
   const carrierOptions = (data, round, optionNum) => {
     const testDataObject = data[round];
     const carrierList = Object.keys(testDataObject);
     return (
-      <div key='Radio-col-child-1' className='Radio-col-child-1'>Carriers:{carrierList.map(value => (
-        <form key={value}>
+      <div key={`Radio-col-child-1-${optionNum}`} className='Radio-col-child-1'>Carriers:{carrierList.map(value => (
+        <form key={`${value}-${optionNum}`}>
           <label>
             <input
               type="radio"
               name={`Radio-Carrier-${optionNum}`}
               value={value}
-              checked={carrier === value}
-              onChange={onChangedCarrier}
+              checked={optionNum === '1' ? carrier === value : carrier2 === value}
+              onChange={optionNum === '1' ? onChangedCarrier : onChangedCarrier2}
             />{value}</label>
         </form>
       ))}</div>
     );
   }
 
-  const phoneModelOptions = (data, round, carrier, optionNum) => {
+  const phoneModelOptions = (data, round, selCarrier, optionNum) => {
     const testDataObject = data[round];
-    if (!(carrier in testDataObject)) {
-      setPhoneModel("");
-      return null;
-    }
-    const phoneModel_list = Object.keys(testDataObject[carrier]);
+    const phoneModel_list = Object.keys(testDataObject[selCarrier]);
     return (
-      <div key='Radio-col-child-2' className='Radio-col-child-2'>Phone Models:{phoneModel_list.map(value => (
-        <form key={value}>
+      <div key={`Radio-col-child-2-${optionNum}`} className='Radio-col-child-2'>Phone Models:{phoneModel_list.map(value => (
+        <form key={`${value}-${optionNum}`}>
           <label>
             <input
               type="radio"
               name={`Radio-Phone-${optionNum}`}
               value={value}
-              checked={phone_model === value}
-              onChange={onChangedPhoneModel}
+              checked={optionNum === '1' ? phone_model === value : phone_model2 === value}
+              onChange={optionNum === '1' ? onChangedPhoneModel : onChangedPhoneModel2}
             />{value}</label>
         </form>
       ))}</div>
     );
   }
 
-  const serverOptions = (data, round, carrier, phoneModel, optionNum) => {
+  const serverOptions = (data, round, selCarrier, selPhoneModel, optionNum) => {
     const testDataObject = data[round];
-    if (carrier === "") {
-      setPhoneModel("");
-      return null;
-    }
-    if (!(phoneModel in testDataObject[carrier])) {
-      setPhoneModel("");
-      setServer("");
-      return null;
-    }
-    const serverList = Object.keys(testDataObject[carrier][phoneModel]);
+    const serverList = Object.keys(testDataObject[selCarrier][selPhoneModel]);
     serverList.pop(); //get rid of total tests field
     return (
-      <div key='Radio-col-child-3' className='Radio-col-child-3'>Server Tests:{
+      <div key={`Radio-col-child-3-${optionNum}`} className='Radio-col-child-3'>Server Tests:{
         serverList.reduce(function (result, value) {
           if (value[value.length - 1] === '1') {
-            result.push(<form key={value}>
+            result.push(<form key={`${value}-${optionNum}`}>
               <label>
                 <input
                   type="radio"
                   name={`Radio-Server-${optionNum}`}
                   value={value}
-                  checked={server === value}
-                  onChange={onChangedServer}
+                  checked={optionNum === '1' ? server === value : server2 === value}
+                  onChange={optionNum === '1' ? onChangedServer : onChangedServer2}
                 />{value}</label>
             </form>);
           }
@@ -151,7 +161,7 @@ function TestOptions() {
     );
   }
 
-  const createButton = (data, round, carrier, phoneModel, server, optionNum) => {
+  const createButton = (data, round, selCarrier, selPhoneModel, selServer, optionNum) => {
     var buttonData = {};
     var descriptionSpeeds;
     var descriptionErrors;
@@ -159,23 +169,39 @@ function TestOptions() {
     buttonData.data = data;
     buttonData.title = {};
     buttonData.title.rounds = round;
-    buttonData.title.carrier = carrier;
-    buttonData.title.phone_model = phoneModel;
-    buttonData.title.server = server;
+    buttonData.title.carrier = selCarrier;
+    buttonData.title.phone_model = selPhoneModel;
+    buttonData.title.server = selServer;
+    buttonData.title.optionNum = optionNum;
     buttonData.title.tests = "speeds";
-    if (server.includes("up")) {
-      descriptionSpeeds = "Upload speeds from ";
-      descriptionErrors = "Upload errors from "
-    } else {
-      descriptionSpeeds = "Download speeds from ";
-      descriptionErrors = "Download errors from ";
+    if (optionNum === '1') {
+      if (server.includes("up")) {
+        descriptionSpeeds = "Upload speeds from ";
+        descriptionErrors = "Upload errors from "
+      } else {
+        descriptionSpeeds = "Download speeds from ";
+        descriptionErrors = "Download errors from ";
+      }
+    } else if (optionNum === '2') {
+      if (server.includes("up")) {
+        descriptionSpeeds = "Compare upload speeds from ";
+        descriptionErrors = "Compare upload errors from "
+      } else {
+        descriptionSpeeds = "Compare download speeds from ";
+        descriptionErrors = "Compare download errors from ";
+      }
     }
-    if (server.includes("wTCP")) {
-      descriptionSpeeds += "West Server";
-      descriptionErrors += "West Server";
-    } else {
-      descriptionSpeeds += "East Server";
-      descriptionErrors += "East Server";
+    if (optionNum === '1') {
+      if (server.includes("wTCP")) {
+        descriptionSpeeds += "West Server";
+        descriptionErrors += "West Server";
+      } else {
+        descriptionSpeeds += "East Server";
+        descriptionErrors += "East Server";
+      }
+    } else if (optionNum === '2') {
+      descriptionSpeeds += "from Graph 1";
+      descriptionErrors += "from Graph 1";
     }
     buttonData.title.description = descriptionSpeeds;
 
@@ -189,15 +215,15 @@ function TestOptions() {
     return (
       <div key="goto-buttons">
         <OptionItem
-          key={`speeds-${server}`}
-          name={server}
+          key={`speeds-${selServer}`}
+          name={selServer}
           description={descriptionSpeeds}
           data={buttonData}
           choice={"Go To Graph"}>
         </OptionItem>
         <OptionItem
-          key={`errors-${server}`}
-          name={server}
+          key={`errors-${selServer}`}
+          name={selServer}
           description={descriptionErrors}
           data={buttonDataErrors}
           choice={"Go To Graph"}>
@@ -205,12 +231,16 @@ function TestOptions() {
       </div>);
   }
 
+  const option1 = radioOptions(location.state.data, location.state.title.rounds, '1');
+  const option2 = radioOptions(location.state.data, location.state.title.rounds, '2');
   return (
     <div className="App">
       <header className="App-header">
         <h2>California Broadband {location.state.title.rounds} Test Options</h2>
         <img src={logo} className="App-logo" alt="logo" />
-        {radioOptions(location.state.data, location.state.title.rounds, "1")}
+        <div>Graph 1 Options:</div>
+        {option1}
+        {option2}
       </header>
     </div>
   );
